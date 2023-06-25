699be8e21a7f6c7a2df539181ad5a67568d41ae6
Vikrant Verma <vikrant.verma2002@gmail.com>
2023-06-25 11:31:29 +0400

    Application Context reuse

--- sample-web/src/test/java/com/sample/web/SampleWebApplicationTests.java
+++ sample-web/src/test/java/com/sample/web/SampleWebApplicationTests.java
@@ -3,7 +3,7 @@ package com.sample.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SampleWebApplicationTests {
 
     @Test
